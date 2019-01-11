8a2e6229633856feb82ca33a4eae69b6ee4e5388
dukedukeduke <46432426+dukedukeduke@users.noreply.github.com>
2019-01-11 22:07:26 +0800

    Update Luckdraw.js

--- static/js/Luckdraw.js
+++ static/js/Luckdraw.js
@@ -121,7 +121,7 @@ function startNum() {
 	num = Math.floor(Math.random() * pcount);
 	nametxt.css('background-image','url('+xinm_new[num]+')');
 	phonetxt.html(phone_new[num]);
-	t = setTimeout(startNum, 0.5);
+	t = setTimeout(startNum, 100);
 }
 
 // 停止跳动
