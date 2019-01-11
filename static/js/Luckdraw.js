508ab26d91d329a90a204a85433a85b9900c013a
dukedukeduke <46432426+dukedukeduke@users.noreply.github.com>
2019-01-11 22:58:28 +0800

    Update Luckdraw.js

--- static/js/Luckdraw.js
+++ static/js/Luckdraw.js
@@ -185,6 +185,7 @@ function zd() {
 						$('.luck-user-list').prepend("<li><div></div><div>"+lotterylevel+"</div></li>");
 					}
 					//将已中奖者从数组中"删除",防止二次中奖
+					console.log(num);
 					console.log("即将删除：");
 					console.log(xinm_new[num]);
 					console.log(phone_new[num]);
